7831355529dec6857ce7a5a179460a50ef531ab9
nacho <nachoherrero93@hotmail.com>
2022-04-08 11:13:27 -0300

    add placeholder

--- pages/index.jsx
+++ pages/index.jsx
@@ -83,7 +83,7 @@ export default function Home() {
                 {...register("article", { required: true, maxLength: 80 })}
                 type="search"
                 className="form-control relative flex-auto min-w-0 block px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                placeholder="Search"
+                placeholder="ej: calabaza, vitaminas, etc."
                 aria-label="Search"
                 aria-describedby="button-addon2"
               />
