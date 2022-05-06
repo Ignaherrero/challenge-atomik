ffed7bd4d0c91b14f8235854abbffc5eb40d48c5
nacho <nachoherrero93@hotmail.com>
2022-05-06 14:00:13 -0300

    modificacion en el dynamic import

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,17 @@
 import dynamic from "next/dynamic";
 import Head from "next/head";
-import { useState } from "react";
+import { Suspense, useState } from "react";
 import { useForm } from "react-hook-form";
 import { statusFetch } from "../helper/constants";
 import { useCounter } from "../helper/useCounter";
-const Card = dynamic(() => import("../components/card"));
-const Pagination = dynamic(() => import("../components/pagination"));
+
 const Search = dynamic(() => import("../components/search"));
+const Card = dynamic(() => import("../components/card"), {
+  ssr: false,
+});
+const Pagination = dynamic(() => import("../components/pagination"), {
+  ssr: false,
+});
 
 export default function Home() {
   const { IDLE } = statusFetch;
@@ -30,25 +35,28 @@ export default function Home() {
         <title>MejorConsalud</title>
         <meta name="description" content="contenido saludable" />
       </Head>
-
-      <Search
-        articles={articles}
-        setArticles={setArticles}
-        handleSubmit={handleSubmit}
-        register={register}
-        reset={reset}
-      />
-
-      <Card articles={articles} />
-
-      <Pagination
-        articles={articles}
-        getValues={getValues}
-        setArticles={setArticles}
-        counter={counter}
-        increment={increment}
-        decrement={decrement}
-      />
+      <Suspense fallback={`...loading`}>
+        <Search
+          articles={articles}
+          setArticles={setArticles}
+          handleSubmit={handleSubmit}
+          register={register}
+          reset={reset}
+        />
+      </Suspense>
+      <Suspense fallback={`...loading`}>
+        <Card articles={articles} />
+      </Suspense>
+      <Suspense fallback={`...loading`}>
+        <Pagination
+          articles={articles}
+          getValues={getValues}
+          setArticles={setArticles}
+          counter={counter}
+          increment={increment}
+          decrement={decrement}
+        />
+      </Suspense>
     </>
   );
 }
